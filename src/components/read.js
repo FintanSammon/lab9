0ef31851602ca47be489785bb6ad575d87d00cd1
FintanSammon <72512712+FintanSammon@users.noreply.github.com>
2023-12-03 16:50:32 +0000

    Update read.js

--- src/components/read.js
+++ src/components/read.js
@@ -1,50 +1,51 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-import Books from "./books";
+// Importing necessary hooks and axios
+import { useEffect, useState } from "react"; // Importing React hooks for state and lifecycle management
+import axios from "axios"; // Importing axios for making HTTP requests
+import Books from "./books"; // Importing the Books component to display a list of books
 
+// Defining the Read functional component
 function Read() {
-   
+    // Initializing state variable to store the books data
     const [data, setData] = useState([]);
 
-  useEffect(
-    ()=>{
-        
-        axios.get('http://localhost:4000/api/books')
-        .then(
-            (response)=>{
+    // useEffect hook to fetch books data when the component mounts
+    useEffect(
+        () => {
+            // Making a GET request to fetch all books data
+            axios.get('http://localhost:4000/api/books')
+            .then((response) => {
+                // Updating the state with the fetched data
                 setData(response.data)
-            }
-        )
-        .catch(
-            (error)=>{
+            })
+            .catch((error) => {
+                // Handling any errors
                 console.log(error);
-            }
-        )
-
-    }, []
-  );
-
-  const ReloadData = (e)=>{
-    axios.get('http://localhost:4000/api/books')
-    .then(
-        (response)=>{
-            setData(response.data)
-        }
-    )
-    .catch(
-        (error)=>{
-            console.log(error);
-        }
+            });
+        }, [] // Empty dependency array to ensure the effect runs only once on mount
     );
-  }
 
+    // Function to reload books data
+    const ReloadData = () => {
+        // Re-fetching all books data
+        axios.get('http://localhost:4000/api/books')
+        .then((response) => {
+            // Updating the state with the new data
+            setData(response.data)
+        })
+        .catch((error) => {
+            // Handling any errors
+            console.log(error);
+        });
+    }
+
+    // JSX for rendering the Read component
     return (
         <div>
             <h2>Hello from Read Component!</h2>
+            {/* Passing the books data and reload function to the Books component */}
             <Books myBooks={data} Reload={ReloadData}></Books>
         </div>
     );
-
 }
 
-export default Read;
+export default Read; // Exporting the Read component for use in other parts of the app
