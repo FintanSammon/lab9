a00af50225fb8662368fbfb60492525a27281cb7
FintanSammon <72512712+FintanSammon@users.noreply.github.com>
2023-12-03 16:42:04 +0000

    Update books.js

--- src/components/books.js
+++ src/components/books.js
@@ -1,14 +1,21 @@
-import BookItem from "./bookItem";
+// Importing the BookItem component
+import BookItem from "./bookItem"; // Importing BookItem to use for displaying each book
 
-function Books(props){
-
-    return props.myBooks.map(
-        (book)=>{
-            
-            return <BookItem myBook={book} key={book._id} reload={()=>{props.Reload()}}></BookItem>
+// Defining the Books functional component
+function Books(props) {
+    // The render part of the component
+    return props.myBooks.map( // Mapping over the array of books passed in through props
+        (book) => {
+            // For each book in the myBooks array, a BookItem component is rendered
+            return (
+                <BookItem 
+                    myBook={book} // Passing the current book object to the BookItem component
+                    key={book._id} // Using the book's _id as a key for React's rendering performance
+                    reload={() => { props.Reload() }} // Passing a reload function to each BookItem
+                ></BookItem>
+            );
         }
     );
-
 }
 
-export default Books;
+export default Books; // Exporting the Books component for use in other parts of the app
